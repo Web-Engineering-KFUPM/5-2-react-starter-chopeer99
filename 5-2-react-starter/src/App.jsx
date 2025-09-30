ee7d5c841b4b8de388637f7527799c2fed2fbffd
chopeer99 <chopeer9999@gmail.com>
2025-09-30 12:09:55 +0300

    pre-check

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
       <main className="dashboard-main">
         <div className="cards-container">
-          <StudentCard name="" id="" dept="" />
+          <StudentCard name = "" id="" dept="" />
          <StudentCard name="" id="" dept="" />
         </div>
       </main>
