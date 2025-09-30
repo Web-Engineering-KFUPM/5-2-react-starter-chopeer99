c98d991bb816af7d44b82c00cfff27c376ea78d7
chopeer99 <chopeer9999@gmail.com>
2025-09-30 11:59:11 +0300

    todo 1-2

--- 5-2-react-starter/src/App.jsx
+++ 5-2-react-starter/src/App.jsx
@@ -10,7 +10,8 @@ function App() {
 
       <main className="dashboard-main">
         <div className="cards-container">
-          {/* TODO: Import and render StudentCard components here */}
+          <StudentCard name="" id="" dept="" />
+         <StudentCard name="" id="" dept="" />
         </div>
       </main>
     </div>
